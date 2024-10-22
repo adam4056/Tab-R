c8aba008de25924bd2fdd19714518d76195f9936
Adam Skřivánek <skrivanek@duck.com>
2024-10-22 14:44:07 +0200

    Major update

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Vytvoření zip archivu s názvem "archiv.zip"
+# -r = rekurzivně projít složky a soubory
+# -x = vynechat soubory odpovídající patternům
+
+zip -r archiv.zip . -x "*.md" ".vscode/*" "LICENSE" "*.sh" ".git/*"
+
+echo "Archiv byl úspěšně vytvořen jako archiv.zip."
